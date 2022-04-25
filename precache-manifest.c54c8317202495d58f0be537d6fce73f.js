8b39fbe9e89c8c554fe91aadf80bd329803fedbe
ThomasNg <147200947@stu.vtc.edu.hk>
2022-04-25 22:36:16 +0800

    Add files via upload

---
+++ precache-manifest.c54c8317202495d58f0be537d6fce73f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "462773841d3d5ada067f1faa76aebbf8",
+    "url": "/LonelyPopFanGame/index.html"
+  },
+  {
+    "revision": "5d35061e45f8101d6957",
+    "url": "/LonelyPopFanGame/static/css/2.f1bd3b54.chunk.css"
+  },
+  {
+    "revision": "4e2489016ee3c0301b8b",
+    "url": "/LonelyPopFanGame/static/css/main.b18b17fe.chunk.css"
+  },
+  {
+    "revision": "5d35061e45f8101d6957",
+    "url": "/LonelyPopFanGame/static/js/2.6b37d824.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/LonelyPopFanGame/static/js/2.6b37d824.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e2489016ee3c0301b8b",
+    "url": "/LonelyPopFanGame/static/js/main.7a36d167.chunk.js"
+  },
+  {
+    "revision": "fd7634fc4bc293116260",
+    "url": "/LonelyPopFanGame/static/js/runtime-main.13d3eaa2.js"
+  },
+  {
+    "revision": "c302908546cfe698e2e8e54334baaae4",
+    "url": "/LonelyPopFanGame/static/media/lonelypop_game-05-min.c3029085.png"
+  },
+  {
+    "revision": "d22985c89da1c1deaaf0d16e35052af7",
+    "url": "/LonelyPopFanGame/static/media/lonelypop_game-06-min.d22985c8.png"
+  }
+]);
